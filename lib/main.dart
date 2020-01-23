51f7299694decca35b8da7dbf74df0b9234583d9
ketthidarat <thidarat.ru.60@ubu.ac.th>
2020-01-23 14:18:45 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  var url = "https://raw.githubusercontent.com//ketthidarat/myproject/master/poke.json";
+  var url = "https://raw.githubusercontent.com/ketthidarat/flutterProject/master/asset/poke.json";
 
 
   @override
@@ -32,11 +32,14 @@ class HomePageState extends State<HomePage> {
 
   PokeHub pokeHub;
 
+
   fetchData() async{
     var res = await http.get(url);
     var decodeValue = jsonDecode(res.body);
 
     pokeHub = pokeHub.fromJson(decodeValue);
+
+    print(pokeHub)   ;
   }
 
   @override
@@ -48,37 +51,12 @@ class HomePageState extends State<HomePage> {
      body: GridView.count(
        crossAxisCount: 2, 
        children: pokeHub.pokemon.map(
-         (Pokemon poke) => Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: Card(
-            elevation: 3.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  height: 100.0,
-                  width: 100.0,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(poke.img)
-                    )
-                  ),
-                ),
-                Text(
-                  poke.name,
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                    ))
-              ],
-            ),
-           ),
-       ))
-       .toList(),
+         (Pokemon poke) => Card()).toList(),
        ),
      drawer: Drawer(),
      floatingActionButton: FloatingActionButton(
        onPressed: () {},
+       child: Icon(Icons.refresh),
      ),
    );
   }
